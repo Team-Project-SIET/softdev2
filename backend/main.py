2e4ebb9b37b295b03dd753926e91077bf53a6ac3
AgnesTachyon-feet <kuysasa007@gmail.com>
2026-08-17 14:39:20 +0700

    test crud

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item1(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.post("/items")
+async def create_item(item: Item1):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item1, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
